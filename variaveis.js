dc3f1605991da12c8f0aef6189fae64776a9a96e
RAPHAEL DE MOURA VIANA <raphael_viana@estudante.sesisenai.org.br>
2025-04-16 22:19:04 -0300

    Declaração  de variável

---
+++ variaveis.js
@@ -0,0 +1,19 @@
+//declaração de variável com var
+
+/*var nome = "Raphael"
+
+ var nome = "Oliver"*/
+
+ //declaração de variável com let
+
+//  let nome = "Raphael"
+
+//  nome = "oliver"
+
+//variável declarada com const
+
+const nome = "Raphael"
+
+nome = "oliver"
+
+ console.log(nome)
